a02d86dad75467811e0071425b8922e0cdfb4f85
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-09-08 12:27:16 -0700

    Update main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-char a[]= "wubishet";
-printf("%s\n", a);
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+char a[]= "wubishet";
+printf("%s\n", a);
     return (0);
 }
