bdcff145355a11f054b1605e49074161c60c31de
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-09-08 12:48:35 -0700

    Update 2-main.c

--- 0x00-hello_world/2-main.c
+++ 0x00-hello_world/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
+ * main - prints the name of file
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("alx-wubishet");
+	printf("%s\n", __FILE__);
 	return (0);
 }
