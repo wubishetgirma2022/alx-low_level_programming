2bbcd2b98f8ce97e88d5e63ae298119b0d81596e
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-09-20 09:23:22 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,17 @@
  */
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+	int count = 0;
 
-	while (str[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+}
