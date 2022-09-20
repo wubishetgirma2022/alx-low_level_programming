095a561989fe4117bbbe49019f9dad91271281ed
Wubishet Girma <111167612+wubishetgirma2022@users.noreply.github.com>
2022-09-20 05:37:05 -0700

    Update 9-main.c

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s1[98];
-    char *ptr;
+  char s1[98];
+  char *p;
 
-    ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", ptr);
-    return (0);
+  p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+  printf("%s", s1);
+  printf("%s", p);
+  return (0);
 }
